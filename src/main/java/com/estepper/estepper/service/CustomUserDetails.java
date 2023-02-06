e6fb6636ab4409575aa7e9dd9732183f81dab69a
Triciabars <pplata@ucm.es>
2023-02-06 13:43:22 +0100

    .

--- src/main/java/com/estepper/estepper/service/CustomUserDetails.java
+++ src/main/java/com/estepper/estepper/service/CustomUserDetails.java
@@ -17,19 +17,16 @@ public class CustomUserDetails implements UserDetails{
     }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public String getPassword() {
-        // TODO Auto-generated method stub
         return user.getContrasenia();
     }
 
     @Override
     public String getUsername() {
-        // TODO Auto-generated method stub
         
         String s=Integer.toString(user.getCodigo());
         return s;
@@ -37,25 +34,21 @@ public class CustomUserDetails implements UserDetails{
 
     @Override
     public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isEnabled() {
-        // TODO Auto-generated method stub
         return true;
     }
 
