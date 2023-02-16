c5ca4e209c75d9580e168091f26b745c82b3d21f
Triciabars <pplata@ucm.es>
2023-02-16 23:44:03 +0100

    Findrisc bien, solo falta hacer el html

--- src/main/java/com/estepper/estepper/repository/FaseValoracionRepository.java
+++ src/main/java/com/estepper/estepper/repository/FaseValoracionRepository.java
@@ -17,4 +17,11 @@ public interface FaseValoracionRepository extends JpaRepository<FaseValoracion, 
     @Transactional
     @Query("UPDATE Exploracion SET primeravez = :primeravez, peso = :peso, talla = :talla, cmcintura = :cmcintura, edad = :edad, imc = :imc WHERE id = :id")
     void updateExploracion(String primeravez, Integer peso, Integer talla, Integer cmcintura, Integer edad, Integer imc, Integer id);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Findrisc SET idParticipante = :idParticipante, puntosedad = :puntosedad, puntosimc = :puntosimc, puntoscmcintura = :puntoscmcintura, ptosactfisica = :ptosactfisica, ptosfrecfruta = :ptosfrecfruta, ptosmedicacion = :ptosmedicacion, ptosglucosa = :ptosglucosa, ptosdiabetes = :ptosdiabetes,  puntuacion = :puntuacion, escalarriesgo = :escalarriesgo WHERE id = :id")
+    void updateFindrisc(Integer id,Integer idParticipante,Integer puntosedad, Integer puntosimc, Integer puntoscmcintura, Integer ptosactfisica,
+    Integer ptosfrecfruta, Integer ptosmedicacion, Integer ptosglucosa, Integer ptosdiabetes, Integer puntuacion,
+    String escalarriesgo);
 }
--- src/main/java/com/estepper/estepper/service/FaseValoracionService.java
+++ src/main/java/com/estepper/estepper/service/FaseValoracionService.java
@@ -7,4 +7,7 @@ public interface FaseValoracionService {
     public List<FaseValoracion> faseValoracion(Integer id);
     public void crearFormularios(Integer id);
     public void updateExploracion(String primeravez, Integer peso, Integer talla, Integer cmcintura, Integer edad, Integer imc, Integer id);
+    public void updateFindrisc(Integer id,Integer idParticipante,Integer puntosedad, Integer puntosimc, Integer puntoscmcintura, Integer ptosactfisica,
+    Integer ptosfrecfruta, Integer ptosmedicacion, Integer ptosglucosa, Integer ptosdiabetes, Integer puntuacion,
+    String escalarriesgo);
 }
