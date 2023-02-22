3e748926d16ba148f3fde48a9f7dcfaeaab51562
almunaha <almunaha@ucm.es>
2023-02-22 22:15:34 +0100

    mejoras

--- src/main/java/com/estepper/estepper/controller/ApiRestController.java
+++ src/main/java/com/estepper/estepper/controller/ApiRestController.java
@@ -10,23 +10,36 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.estepper.estepper.model.entity.Usuario;
+import com.estepper.estepper.model.enums.TipoProgreso;
+import com.estepper.estepper.model.entity.Participante;
+
+import com.estepper.estepper.model.entity.Progreso;
 import com.estepper.estepper.service.UsuarioService;
+import com.estepper.estepper.service.ParticipanteService;
+import com.estepper.estepper.service.ProgresoService;
 
 //Controlador de recursos
 //Controlador para API REST que se encarga de las solicitudes HTTP entrantes para luego llamar a un servicio
 //y coger los datos de la base de datos
+//se necesita estar logueado hay que mirar eso
 @RestController
 public class ApiRestController {
 
     @Autowired
     private UsuarioService usuario;
 
-    @GetMapping("/usuarios")//siempre que accedamos a /usuarios accedera al listado y lo ejecutará
-    public List<Usuario> listado(){
+    @Autowired
+    private ParticipanteService part;
+
+    @Autowired
+    private ProgresoService progreso;
+
+    @GetMapping("/usuarios") // siempre que accedamos a /usuarios accedera al listado y lo ejecutará
+    public List<Usuario> listado() {
         return usuario.listadoTotal();
     }
 
-    public Usuario getUsuario(){
+    public Usuario getUsuario() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         UserDetails userDetails = null;
@@ -34,12 +47,17 @@ public class ApiRestController {
             userDetails = (UserDetails) principal;
         }
 
-        String codigo = userDetails.getUsername(); //codigo del logueado
+        String codigo = userDetails.getUsername(); // codigo del logueado
 
         Usuario user = usuario.logueado(Integer.parseInt(codigo));
         return user;
     }
 
- 
-    
+    @GetMapping("/progreso/peso")
+    public List<Progreso> progresoPeso() {
+        Participante p = part.findById(25).get();
+
+        return progreso.datos(p, TipoProgreso.PESO);
+    }
+
 }
--- src/main/java/com/estepper/estepper/repository/ProgresoRepository.java
+++ src/main/java/com/estepper/estepper/repository/ProgresoRepository.java
@@ -1,11 +1,16 @@
 package com.estepper.estepper.repository;
 import com.estepper.estepper.model.entity.Progreso;
+import com.estepper.estepper.model.enums.TipoProgreso;
+import com.estepper.estepper.model.entity.Participante;
 
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ProgresoRepository extends JpaRepository<Progreso, Integer>{
+    List<Progreso> findByParticipanteAndTipo(Participante participante, TipoProgreso tipo);
     
 }
