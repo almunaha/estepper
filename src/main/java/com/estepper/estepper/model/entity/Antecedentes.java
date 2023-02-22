c15f5775926ad77dfac06cf83f175ea1aeac99b6
Triciabars <pplata@ucm.es>
2023-02-22 17:18:05 +0100

    antecedentes menos formulario html

---
+++ src/main/java/com/estepper/estepper/model/entity/Antecedentes.java
@@ -0,0 +1,135 @@
+package com.estepper.estepper.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "antecedentes")
+public class Antecedentes extends FaseValoracion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
+
+    public String hta;
+    public String tiroides;
+    public String patmental;
+    public String dislipemias;
+    public String patmuscesq;
+    public String medicacion;
+    public String fuma;
+    public String dejardefumar;
+    public Integer tasistolica;
+    public Integer tadiastolica;
+
+    public Antecedentes() {
+        super();
+    }
+
+    public Antecedentes(Integer id, Participante participante, String hta, String tiroides, String patmental, String dislipemias, String patmuscesq, String medicacion, String fuma, String dejardefumar, Integer tasistolica, Integer tadiastolica) {
+        super(id, participante);
+        this.hta = hta; 
+        this.tiroides = tiroides;
+        this.patmental = patmental;
+        this.dislipemias = dislipemias;
+        this.patmuscesq = patmuscesq;
+        this.medicacion = medicacion;
+        this.fuma = fuma;
+        this.dejardefumar = dejardefumar;
+        this.tasistolica = tasistolica;
+        this.tadiastolica = tadiastolica;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getHta() {
+        return hta;
+    }
+
+    public void setHta(String hta) {
+        this.hta = hta;
+    }
+
+    public String getTiroides() {
+        return tiroides;
+    }
+
+    public void setTiroides(String tiroides) {
+        this.tiroides = tiroides;
+    }
+
+    public String getPatmental() {
+        return patmental;
+    }
+
+    public void setPatmental(String patmental) {
+        this.patmental = patmental;
+    }
+
+    public String getDislipemias() {
+        return dislipemias;
+    }
+
+    public void setDislipemias(String dislipemias) {
+        this.dislipemias = dislipemias;
+    }
+
+    public String getPatmuscesq() {
+        return patmuscesq;
+    }
+
+    public void setPatmuscesq(String patmuscesq) {
+        this.patmuscesq = patmuscesq;
+    }
+
+    public String getMedicacion() {
+        return medicacion;
+    }
+
+    public void setMedicacion(String medicacion) {
+        this.medicacion = medicacion;
+    }
+
+    public String getFuma() {
+        return fuma;
+    }
+
+    public void setFuma(String fuma) {
+        this.fuma = fuma;
+    }
+
+    public String getDejardefumar() {
+        return dejardefumar;
+    }
+
+    public void setDejardefumar(String dejardefumar) {
+        this.dejardefumar = dejardefumar;
+    }
+
+    public Integer getTasistolica() {
+        return tasistolica;
+    }
+
+    public void setTasistolica(Integer tasistolica) {
+        this.tasistolica = tasistolica;
+    }
+
+    public Integer getTadiastolica() {
+        return tadiastolica;
+    }
+
+    public void setTadiastolica(Integer tadiastolica) {
+        this.tadiastolica = tadiastolica;
+    }
+
+    
+
+}
--- src/main/java/com/estepper/estepper/service/FaseValoracionServiceImpl.java
+++ src/main/java/com/estepper/estepper/service/FaseValoracionServiceImpl.java
@@ -11,6 +11,7 @@ import com.estepper.estepper.model.entity.Participante;
 import com.estepper.estepper.model.enums.Estado;
 import com.estepper.estepper.model.enums.Sexo;
 import com.estepper.estepper.model.entity.Exploracion;
+import com.estepper.estepper.model.entity.Antecedentes;
 import com.estepper.estepper.model.entity.Clasificacion;
 import com.estepper.estepper.repository.FaseValoracionRepository;
 import com.estepper.estepper.repository.ExploracionRepository;
@@ -18,6 +19,7 @@ import com.estepper.estepper.repository.FindriscRepository;
 import com.estepper.estepper.repository.ParticipanteRepository;
 import com.estepper.estepper.repository.UsuarioRepository;
 import com.estepper.estepper.repository.ClasificacionRepository;
+import com.estepper.estepper.repository.AntecedentesRepository;
 
 
 @Service
@@ -41,6 +43,9 @@ public class FaseValoracionServiceImpl implements FaseValoracionService {
     @Autowired
     private ClasificacionRepository repoC;
 
+    @Autowired
+    private AntecedentesRepository repoA;
+
     @Override
     public List<FaseValoracion> faseValoracion(Participante participante) {
         return repoV.findByParticipante(participante);
@@ -59,13 +64,16 @@ public class FaseValoracionServiceImpl implements FaseValoracionService {
     @Override
     public void crearFormulariosNuevos(Participante participante){
         //COMPROBAR QUE NO ESTÉN CREADOS YA
-
-        //CLASIFICACION
-        Clasificacion clasificacion = new Clasificacion(0, participante, "no", 0, 0, 0, 0, 0, 0, 0.0, "no", 0, "no", "", "no", "", "no" );
-        repoC.save(clasificacion);
-        //ANTECEDENTES
-        //ALIMENTACION
-        //ACTIVIDAD FISICA
+        if(repoC.findByParticipante(participante) == null){
+            //CLASIFICACION
+            Clasificacion clasificacion = new Clasificacion(0, participante, "no", 0, 0, 0, 0, 0, 0, 0.0, "no", 0, "no", "", "no", "", "no" );
+            repoC.save(clasificacion);
+            //ANTECEDENTES
+            Antecedentes antecedentes = new Antecedentes(0, participante, "no", "no", "no", "no", "no", "no", "no", "no", 0, 0);
+            repoA.save(antecedentes);
+            //ALIMENTACION
+            //ACTIVIDAD FISICA
+        }
     }
 
     @Override
@@ -135,6 +143,7 @@ public class FaseValoracionServiceImpl implements FaseValoracionService {
         repoE.deleteByParticipante(participante);
         repoF.deleteByParticipante(participante);
         repoC.deleteByParticipante(participante);
+        repoA.deleteByParticipante(participante);
         repoP.delete(participante);
     }
 }
