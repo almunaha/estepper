282be4b4f43460cc1d070e428b90520b1baaa435
almunaha <almunaha@ucm.es>
2023-02-23 02:55:16 +0100

    progreso

---
+++ src/main/resources/static/js/progreso.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    //Botones del PESO
+    $(".pesoTabla").hide();
+    $("#form-peso").hide();
+    $("#grafica").show();
+
+    $("#registroPeso").click(function () {
+        $(".pesoTabla").show();
+        $("#grafica").hide();
+        $("#form-peso").hide();
+    });
+
+    $("#graficaPeso").click(function () {
+        $("#grafica").show();
+        $(".pesoTabla").hide();
+        $("#form-peso").hide();
+     });
+
+     $(".btn-plus").click(function () {
+        $("#grafica").hide();
+        $(".pesoTabla").hide();
+        $("#registroPeso").hide();
+        $("#graficaPeso").hide();
+        $(".btn-plus").hide();
+        $("#form-peso").show();
+     });
+
+
+    
+   });
