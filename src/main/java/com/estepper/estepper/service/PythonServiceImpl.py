56ef0bddbf63d761107a199c3f44ec9bec1aa2f2
Triciabars <pplata@ucm.es>
2023-03-24 18:01:22 +0100

    se me olvidó este

--- src/main/java/com/estepper/estepper/service/PythonServiceImpl.py
+++ src/main/java/com/estepper/estepper/service/PythonServiceImpl.py
@@ -8,6 +8,11 @@ class PythonServiceImpl(PythonService):
         return self.value
     
     def recetasparecidas(self, want, dontwant):
-        # from com.ziclick.python.sql import ZXJDBC
-        # db = ZXJDBC.connect()
+        # CONECTAR BASE DE DATOS Y EXTRAER LISTA DE ALIMENTOS Y RECETAS
+            # from com.ziclick.python.sql import ZXJDBC
+            # db = ZXJDBC.connect()
+        # DESCARTAR ALIMENTOS QUE ESTÉN EN DONTWANT
+        # HACER CLUSTER CON ALIMENTOS
+        # COGER ALIMENTOS QUE ESTÉN EN EL MISMO CLASTER QUE LOS WANTS
+        # COGER RECETAS CON ALGUNO DE ESOS ALIMENTOS
         return ["1", "2", "3"]
