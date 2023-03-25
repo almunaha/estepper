a4a5b624667c3e1569637a5f87d8b4f3c5a9f461
almunaha <almunaha@ucm.es>
2023-03-25 17:25:10 +0100

    .

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,6 +1,8 @@
 function desplazar() {
 
     let sidebar = document.querySelector("#sidebar-container"); //barra lateral izquierda
+    let header = document.querySelector("#header");
    
     sidebar.classList.toggle("active"); //cambiar botón a activo
+    header.classList.toggle("active");
 }
