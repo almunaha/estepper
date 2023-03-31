a5f2fd2ba74b6847035829d1df511938b2294194
Triciabars <pplata@ucm.es>
2023-03-31 13:25:34 +0200

    para que podais seguir usandolo sin python

--- src/main/java/com/estepper/estepper/service/PythonServiceImpl.py
+++ src/main/java/com/estepper/estepper/service/PythonServiceImpl.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from com.estepper.estepper.service import PythonService
-import jaydebeapi
+# import jaydebeapi
 # from sklearn.feature_extraction.text import CountVectorizer
 # from sklearn.metrics.pairwise import cosine_similarity
 
@@ -12,49 +12,49 @@ class PythonServiceImpl(PythonService):
         return self.value
 
     def recetasparecidas(self, want, dontwant):
-        # CONECTAR BASE DE DATOS Y EXTRAER LISTA DE ALIMENTOS Y RECETAS
+        # # CONECTAR BASE DE DATOS Y EXTRAER LISTA DE ALIMENTOS Y RECETAS
 
-        # Conectar a la base de datos
-        conn = jaydebeapi.connect(
-            "com.mysql.jdbc.Driver",
-            "jdbc:mysql://localhost:3306/estepper",
-            ["estepper", "estepper"],
-        )
+        # # Conectar a la base de datos
+        # conn = jaydebeapi.connect(
+        #     "com.mysql.jdbc.Driver",
+        #     "jdbc:mysql://localhost:3306/estepper",
+        #     ["estepper", "estepper"],
+        # )
 
-        # Obtener los alimentos y recetas
-        alimentos = []
-        with conn.cursor() as cur:
-            cur.execute(
-                "SELECT nombre, sal, proteinas, hidratos_de_carbono, fibra_alimentaria, grasas_saturadas, id FROM alimentacion"
-            )
-            for row in cur:
-                alimento = (
-                    row[0],
-                    row[1],
-                    row[2],
-                    row[3],
-                    row[4],
-                    row[5],
-                    row[6].split(","),
-                )
-                alimentos.append(alimento)
+        # # Obtener los alimentos y recetas
+        # alimentos = []
+        # with conn.cursor() as cur:
+        #     cur.execute(
+        #         "SELECT nombre, sal, proteinas, hidratos_de_carbono, fibra_alimentaria, grasas_saturadas, id FROM alimentacion"
+        #     )
+        #     for row in cur:
+        #         alimento = (
+        #             row[0],
+        #             row[1],
+        #             row[2],
+        #             row[3],
+        #             row[4],
+        #             row[5],
+        #             row[6].split(","),
+        #         )
+        #         alimentos.append(alimento)
 
-        recetas = []
-        with conn.cursor() as cur:
-            cur.execute("SELECT nombre, id FROM recetas")
-            for row in cur:
-                receta = (row[0], row[1].split(","))
-                recetas.append(receta)
+        # recetas = []
+        # with conn.cursor() as cur:
+        #     cur.execute("SELECT nombre, id FROM recetas")
+        #     for row in cur:
+        #         receta = (row[0], row[1].split(","))
+        #         recetas.append(receta)
 
-        ingredientes = []
-        with conn.cursor() as cur:
-            cur.execute("SELECT receta_id, alimentacion_id FROM receta_alimentacion")
-            for row in cur:
-                ingrediente = (row[0], row[1].split(","))
-                ingredientes.append(ingrediente)
+        # ingredientes = []
+        # with conn.cursor() as cur:
+        #     cur.execute("SELECT receta_id, alimentacion_id FROM receta_alimentacion")
+        #     for row in cur:
+        #         ingrediente = (row[0], row[1].split(","))
+        #         ingredientes.append(ingrediente)
 
-        # Cerrar la conexión
-        conn.close()
+        # # Cerrar la conexión
+        # conn.close()
 
         # # DESCARTAR ALIMENTOS QUE ESTÉN EN DONTWANT
         # # HACER CLUSTER CON ALIMENTOS HACIENDO LA CLASIFICACIÓN EN FUNCIÓN DE LA SIMILITUD DE COSENO PARA VER CÓMO DE PARECIDOS SON
