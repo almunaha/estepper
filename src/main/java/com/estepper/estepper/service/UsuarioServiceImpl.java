7c353772375efd50735aa87a088e58319b625f4c
Triciabars <pplata@ucm.es>
2023-04-02 22:34:19 +0200

    correo mejoras

--- src/main/java/com/estepper/estepper/service/UsuarioServiceImpl.java
+++ src/main/java/com/estepper/estepper/service/UsuarioServiceImpl.java
@@ -3,7 +3,6 @@ package com.estepper.estepper.service;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Paths;
 
 import java.util.List;
 import java.util.Optional;
@@ -38,7 +37,7 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.gmail.GmailScopes;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.model.Message;
-
+import com.google.api.services.gmail.model.MessagePart;
 import com.estepper.estepper.model.enums.Estado;
 
 @Service
@@ -111,42 +110,44 @@ public class UsuarioServiceImpl implements UserDetailsService, UsuarioService{
     private static Credential getCredentials(final NetHttpTransport httpTransport, GsonFactory jsonFactory) throws IOException{
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(UsuarioServiceImpl.class.getResourceAsStream("/client_secret_997788153381-j7h2r75bek6g35no8jmsmfq729cgc1g1.apps.googleusercontent.com.json")));
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, jsonFactory, clientSecrets, Set.of(GmailScopes.GMAIL_SEND))
-        .setDataStoreFactory(new FileDataStoreFactory(Paths.get("tokens").toFile()))
+        .setDataStoreFactory(new FileDataStoreFactory(new java.io.File("tokens")))
         .setAccessType("offline")
         .build();
 
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("Estepper");
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
         return credential;
     }
     private void mandarcorreo(String correo, String bodyText) throws Exception {
-       String messageSubject = "Recuperación de datos - Estepper";
+        String messageSubject = "Recuperación de datos - Estepper";
         NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
         GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
         Gmail service = new Gmail.Builder(httpTransport, jsonFactory, getCredentials(httpTransport, jsonFactory))
-        .setApplicationName("Estepper")
-        .build();
-
-        Properties props = new Properties();
-        Session session = Session.getDefaultInstance(props);
-        // Define message
-        MimeMessage message = new MimeMessage(session);
-        message.setFrom(new InternetAddress(correoEstepper));
-        message.setSubject(messageSubject);
-        message.addRecipient(javax.mail.Message.RecipientType.TO,new InternetAddress(correo));
-        message.setText(bodyText);
-
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        message.writeTo(buffer);
-        byte[] rawMessageBytes = buffer.toByteArray();
-        String encodedEmail = Base64.encodeBase64URLSafeString(rawMessageBytes);
-        Message email = new Message();
-            email.setRaw(encodedEmail);
+            .setApplicationName("Estepper")
+            .build();
+            Properties props = new Properties();
+            Session session = Session.getDefaultInstance(props);
+            // Define message
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(correoEstepper));
+            message.setSubject(messageSubject);
+            message.addRecipient(javax.mail.Message.RecipientType.TO,new InternetAddress(correo));
+            message.setText(bodyText);
+    
+            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+            message.writeTo(buffer);
+            byte[] rawMessageBytes = buffer.toByteArray();
+            String encodedEmail = Base64.encodeBase64URLSafeString(rawMessageBytes);
+            Message email = new Message();
+                email.setRaw(encodedEmail);
         try {
-            email = service.users().messages().send("other", email).execute();
+            email = service.users().messages().send("me", email).execute();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        
                 
     }
+
+    
 }
