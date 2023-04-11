9e4622514b6a8eb43a1d29607e3627bf0fd71167
almunaha <almunaha@ucm.es>
2023-04-11 19:13:17 +0200

    base juego

---
+++ src/main/java/com/estepper/estepper/repository/AlimentoIntercambioRepository.java
@@ -0,0 +1,10 @@
+package com.estepper.estepper.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estepper.estepper.model.entity.AlimentoIntercambio;
+
+public interface AlimentoIntercambioRepository  extends JpaRepository<AlimentoIntercambio, Integer>{
+
+    
+}
