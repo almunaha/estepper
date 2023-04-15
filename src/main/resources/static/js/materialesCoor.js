590996c1506d1511f482a7e3a2c083a571b31e24
Triciabars <pplata@ucm.es>
2023-04-15 16:45:01 +0200

    materiales Coor y Grupo js

---
+++ src/main/resources/static/js/materialesCoor.js
@@ -0,0 +1,48 @@
+function abrirIframe(modalId) {
+    const modal = new bootstrap.Modal(document.getElementById(modalId));
+    modal.show();
+}
+
+$(document).ready(function () {
+    (function ($) {
+        $('#filtrarMateriales').keyup(function () {
+            var rex = new RegExp($(this).val(), 'i');
+            $('.buscarParticipantes tr').hide();
+            $('.buscarParticipantes tr').filter(function () {
+                return rex.test($(this).text());
+            }).show();
+        })
+    }(jQuery));
+
+    $('.eliminar').click(function () {
+        var id = $(this).data('id');
+
+        Swal.fire({
+            position: 'center',
+            title: '<h4>¿Estás seguro de eliminar el material?</h4>',
+            showConfirmButton: true,
+            showCancelButton: true,
+            cancelButtonText: 'Cancelar',
+            confirmButtonColor: "rgb(218, 77, 73)",
+            confirmButtonText: '<a href="/eliminarMaterial/' + id + '" id ="conf">Eliminar</a>',
+
+            didRender: function () {
+                const confirm = document.querySelector('#conf');
+
+                if (confirm) {
+                    confirm.style.color = 'white';
+                }
+            },
+
+            showClass: {
+                popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__fadeOutUp'
+            }
+
+        })
+
+    });
+
+});
