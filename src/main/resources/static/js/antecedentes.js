dc859c62886d3f2fa95a7f2419cebc00ba0c4085
Triciabars <pplata@ucm.es>
2023-04-15 09:40:44 +0200

    fase de valoracion a js

---
+++ src/main/resources/static/js/antecedentes.js
@@ -0,0 +1,8 @@
+
+function myFunction() {
+    if (confirm("Desea guardar los datos?")) {
+        alert("Datos guardados exitosamente");
+    } else {
+        alert("Usted cancelo la acción para guardar");
+    }
+}
